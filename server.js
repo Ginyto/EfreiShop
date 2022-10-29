ab5eddf07db27f40647372b09d35cb885139b0d5
Ginyto <agianlucca@gmail.com>
2022-10-29 20:36:45 +0200

    server test

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ app.get(/.*/, (req, res) => {
 
 
 
-app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) })
+app.listen(PORT,"0.0.0.0", () => { console.log(`Server started on port ${PORT}`) })
