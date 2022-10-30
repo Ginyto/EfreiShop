031d89df468afe73cb2a46d1965ec4f6d39111c0
Justinewg <73750498+Justinewg@users.noreply.github.com>
2022-10-30 19:44:22 +0100

    router add sneaker

--- routes/cart.js
+++ routes/cart.js
@@ -12,7 +12,6 @@ const { Op } = require("sequelize");
 //Importing the models
 const { User } = require('../sequelize');
 const { Sneaker } = require('../sequelize');
-const { Cart } = require('../sequelize');
 
 
 
@@ -24,38 +23,3 @@ router.get('/', (req, res) => {
     res.send('API is working')
 })
 
-
-//GET
-router.get('/sneakers', (req, res) => {
-
-    Sneaker.findAll().then(sneakers => {
-        res.send(sneakers)
-        
-    }, (err) => {
-        res.status(500).send(err.message)
-    })
-
-})
-
-
-//POST 
-router.post('/addsneaker',(req, res) => {
-    Cart.create({ 
-        userID: 1,
-        sneakerID : 1
-    })  .then()
-        .catch(err => {
-        res.send(err)
-    }
-    )
-})
-
-
-
-
-
-
-
-
-
-module.exports = router;
