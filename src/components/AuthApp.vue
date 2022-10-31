67f766c82be34359dd562eaeca6046a0b9e55902
Biribin <lineo.biribin@efrei.net>
2022-10-31 13:45:13 +0100

    pop up avec bouton

--- src/components/AuthApp.vue
+++ src/components/AuthApp.vue
@@ -1,24 +1,40 @@
 <template>
   <div id="authzone">
+    <h1  v-show=login >Welcome {{user.name}} </h1>
     <input type="text" class="saisieInput" v-model.lazy="mail" v-show= !login placeholder="Mail">
    
-    <input type="text" class="saisieInput" v-model.lazy="name"  v-show= login  placeholder="Name">
-
-        <input type="text" class="saisieInput" v-model.lazy="lastname" v-show= login  placeholder="Lastname">
 
         <input type="password" class="saisieInput" v-model.lazy="password" v-show= !login placeholder="Password">
       
-        <input type="password" class="saisieInput" v-model.lazy="passwordd" v-show= login 
-          placeholder="Confirm your Password">
-      
-          <button class="btn" @click=uploadUser() v-show=login>  sign up</button>
-          <button class="btn" @click="loginUser()" v-show=!login>  log in </button>
-          <h1>Welcome {{user.name}} </h1>
-  </div><div>
+       
    
+          
+          <button class="btn" @click=loginUser() v-show=!login>  log in </button>
+         
+           
+  </div><div>
+    <button class="btn" @click=showModall()>showModal </button>
+          <transition name="fade" appear>
+        <div class="modal-overlay" v-if="!showModal" v-show=!showModal ></div>
+          </transition>
+          <Transition name="slide" appear  v-show=!showModal>
+            <div class="modal" >
+            <input type="text" class="saisieInput" v-model.lazy="mail" placeholder="Mail">
+            <div></div>
+            <input type="text" class="saisieInput" v-model.lazy="name"  placeholder="Name">
+            <div></div>
+           <input type="text" class="saisieInput" v-model.lazy="lastname"   placeholder="Lastname">
+           <div></div>
+        <input type="password" class="saisieInput" v-model.lazy="password"  placeholder="Password">
+        <div></div>
+        <input type="password" class="saisieInput" v-model.lazy="passwordd" 
+          placeholder="Confirm your Password">
+          <button class="btn" @click=uploadUser() >  sign up</button>
+           
+        </div>
+          </Transition>
   </div>
- 
-  <widget-container-modal />
+  
 </template>
 
 
@@ -26,20 +42,17 @@
 
 
 import axios from 'axios';
-import router from '../router/index'
+
 
 
 export default {
-
-    
-
 
   created() {},
   data() {
     return {
 
-    
-    
+
+    showModal:true,
       close:true,
       login : false,
        mail: null,
@@ -59,8 +72,14 @@ export default {
 
   methods: {
    
-    close(){
-      close=!close;
+    showModall(){
+      this.showModal = !this.showModal;
+      console.log("ça change")
+    },
+
+    logmode() {
+      this.login = !this.login
+    
     },
 
     async uploadUser() {
@@ -74,7 +93,8 @@ export default {
       mail: this.mail,
       name: this.name,
       lastname: this.lastname,
-      password: this.password
+      password: this.password,
+     
 
     }
 
@@ -88,6 +108,7 @@ export default {
       }
       else {
         alert('You are now registered')
+        this.showModall()
       
         this.mail = ''
         this.name = ''
@@ -118,14 +139,14 @@ export default {
 
 
 
-    async loginUser() {
+async loginUser() {
 
 if (this.mail != '' && this.password != '') {
 
-  await axios.post(this.url + 'login', {}, {
+  await axios.post(this.url + '/login', {}, {
 
     params: {
-     
+
       mail: this.mail,
       password: this.password,
 
@@ -138,7 +159,8 @@ if (this.mail != '' && this.password != '') {
 
     console.log(response)
 
-    
+    this.role = currentUser.role
+
     const user = {
 
       id: currentUser.id,
@@ -155,14 +177,13 @@ if (this.mail != '' && this.password != '') {
     // console.log(user)
 
     sessionStorage.setItem('user', JSON.stringify(user))
-    this.user =  JSON.parse(sessionStorage.getItem('user'))
 
+    this.user=  JSON.parse(sessionStorage.getItem('user'))
 
-    alert('you are connected')
-
+    this.logmode()
   }).catch(error => {
 
-    alert('mail or password is incorrect')
+    alert('Identification or password is incorrect')
     console.log(error)
   })
 
@@ -173,25 +194,77 @@ else {
 
 }
 },
-  },
+
+  
+ },
 
 };
 </script>
 
 <style lang="css" scoped>
 
+.modal-overlay{
+  position: absolute;
+  top:0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index:98;
+  background-color: rgba(0, 0,0 ,0.3);
+
+
+}
+h1{
+  color:rgb(255, 255, 255);
+  font-size:32px;
+  font-weight: 900;
+  margin-bottom: 15px;
+}
+.modal{
+  position:fixed;
+  top:50%;
+  left: 50%;
+  transform: transltae(-50%,-50%);
+  z-index:99;
+
+  width :100%;
+  max-width:300px;
+  background-color:  rgba(0, 0,0 ,0.85);
+  border-radius: 16px;
+
+  padding : 25px;
+}
+.fade-enter-active,
+.fade-leaave-active{
+
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to{
+  opacity: 0;
+}
+.slide-enter-active,
+.slide-leaave-active{
+
+  transition: transform 10s;
+}
+.slide-enter,
+.slide-leave-to{
+transform : translateY(-50%) translateX(100vw);
+}
+
 #authzone {
   display: flex;
   width: fit-content;
   height: fit-content;
   padding: 1.5vh;
-  background-color: rgb(45, 30, 30);
+  background-color: rgb(34, 34, 34);
   justify-content: center;
   align-items: center;
   flex-direction: column;
 }
 .btn {
-  width: 30vh;
+  width: 15vh;
   height: fit-content;
 
   border-radius: 1vh;
@@ -202,15 +275,23 @@ else {
 
   font-size: 2vh;
 
-  padding: 1vh;
+ 
 
   margin: 1vh;
 
   cursor: pointer;
 
   background: linear-gradient(to left, #2a2c2e, #c3c3c3);
+  appearance:none;
+  display: inline-block;
+  padding :15px 25px;
+
+
+
 
 
 }
+
+
 
 </style>
